92692b46e9d2927fec7e8b4ffc8fae5a6d519a01
thetakeaway <jczimm.alias+@gmail.com>
2014-03-18 08:49:14 -0400

    fixed .nth() (.nths()), added .evens() + .odds()

--- zeus.js
+++ zeus.js
@@ -10,19 +10,29 @@ window.zeus = (function(){
     return output;
   }
    
-  Array.prototype.nth = function(str){
-  	var a = (str=="even"||str=="odd" ? 2 : str.match(/\dn(\+\d)?/i) ? str.match(/\dn(\+\d)?/i)[0][0] : str.match(/\d/i) ? 0 : 0);
-  	var b = (str=="even" ? 1 : str.match(/\dn(\+\d)?/i) ? str.match(/\dn(\+\d)?/i)[1][1] : str.match(/\d/i) ? str.match(/\d/i)[0]+"");
-  	var output = [];
-  	
-  	if(a){
-    	for(i=0;i<this.length;i++){
-    		if(i%a==0){output.push(this[i+b]);}
-    	}
-  	}else{
-  	  output.push(this[b]);
-  	}
-  	return output;
+  Array.prototype.nths = function(a,b){
+	  var output = [];
+	  for(i=0;i<this.length;i++){
+		  if(i%a==0){output.push(this[i+b]);}
+	  }
+	  return output;
+  }
+  
+  Array.prototype.evens = function(){
+    var output = [];
+	  for(i=0;i<this.length;i++){
+		  if(i%2==0){output.push(this[i+1]);}
+	  }
+	  if(output[output.length-1]==undefined){output = output.slice(0,output.length-1);}
+	  return output;
+  }
+  
+  Array.prototype.odds = function(){
+    var output = [];
+	  for(i=0;i<this.length;i++){
+		  if(i%2==0){output.push(this[i]);}
+	  }
+	  return output;
   }
 
 }());
